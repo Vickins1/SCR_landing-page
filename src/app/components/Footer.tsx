30a904118f574b3a633745ff8a656459c8b9fc58
Vickins1 <vickinstechnologies@gmail.com>
2025-07-07 18:06:00 +0300

    updates4

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaFacebookF, FaXTwitter, FaInstagram, FaLinkedinIn, FaTiktok } from "react-icons/fa6";
+import { FaFacebookF, FaXTwitter, FaInstagram, FaTiktok } from "react-icons/fa6";
 
 export default function Footer() {
   return (
